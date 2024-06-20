d96d10a1de901753c746e32f4d72938da2f1d781
Denis <denis.alekseev.200411@mail.ru>
2024-06-20 19:49:52 +0300

    wpf project add

---
+++ LibraryCloudDB/LibraryCloudDB/CloudDBClient.cs
@@ -0,0 +1,41 @@
+﻿using LibraryCloudDB.CreateTable;
+using LibraryCloudDB.Http;
+using LibraryCloudDB.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCloudDB
+{
+    public class CloudDBClient
+    {
+        private readonly string baseUrl;
+        private readonly string secretToken;
+
+        public CloudDBClient(AuthorizationClient authorizationClient)
+        {
+            baseUrl = authorizationClient.baseUrl;
+            secretToken = authorizationClient.secretToken;
+
+            if (!baseUrl.EndsWith("/"))
+            {
+                baseUrl += "/";
+            }
+        }
+
+        public CloudDBQuery Child(string nameTable)
+        {
+            var tableName = nameTable + '/';
+
+            return new CloudDBQuery(secretToken, baseUrl + tableName);
+        }
+
+        public async Task<string> CreateTable(string nameTable, IEnumerable<CreateTableObject.Field> tableObjects)
+        {
+            return await HttpClientExtensions.PostObjectAsync(new HttpClient(), baseUrl + "create_table?table_name=" + nameTable + "&token=" + secretToken, tableObjects);
+        }
+    }
+}
---
+++ LibraryCloudDB/LibraryCloudDB/CreateTable/CreateField.cs
@@ -0,0 +1,36 @@
+﻿namespace LibraryCloudDB.CreateTable
+{
+    public class CreateField : CreateTableObject
+    {
+        public CreateField(Field field, References references)
+        {
+            this.field = field;
+            this.references = references;
+        }
+
+        public CreateRelationships TypeField(Type type)
+        {
+            switch (type)
+            {
+                case Type.INTEGER:
+                    field.Type_field = "INTEGER";
+                    break;
+                case Type.TEXT:
+                    field.Type_field = "TEXT";
+                    break;
+                case Type.REAL:
+                    field.Type_field = "REAL";
+                    break;
+                case Type.NUMERIC:
+                    field.Type_field = "NUMERIC";
+                    break;
+                case Type.BLOB:
+                    field.Type_field = "BLOB";
+                    break;
+            }
+
+
+            return new CreateRelationships(field, references);
+        }
+    }
+}
