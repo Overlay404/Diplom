a88526caa0ecb4e767a71b43015fe18480fa74f8
Denis <denis.alekseev.200411@mail.ru>
2024-06-20 19:52:49 +0300

    wpf project add

---
+++ PresentationLibraryFunctions/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using LibraryCloudDB;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PresentationLibraryFunctions
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static AuthorizationClient authorizationClient = new AuthorizationClient("e1b7e325f5460f023c9e993e36070130", "http://127.0.0.1:8000/api");
+        public static CloudDBClient cloudDBClient = new CloudDBClient(authorizationClient);
+    }
+}
---
+++ PresentationLibraryFunctions/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using PresentationLibraryFunctions.Pages;
+using System;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace PresentationLibraryFunctions
+{
+    public partial class MainWindow : Window
+    {
+
+
+        public int PageNow
+        {
+            get { return (int)GetValue(PageNowProperty); }
+            set { SetValue(PageNowProperty, value); }
+        }
+
+        public static readonly DependencyProperty PageNowProperty =
+            DependencyProperty.Register("PageNow", typeof(int), typeof(MainWindow));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NavigateToLeftButton.MouseDown += NavigateToLeft;
+            NavigateToRightButton.MouseDown += NavigateToRight;
+            Navigate();
+        }
+
+        public void NavigateToLeft(object sender, RoutedEventArgs e)
+        {
+            if (PageNow <= 0)
+            {
+                PageNow = 0;
+                return;
+            }
+
+            PageNow -= 1;
+            try
+            {
+                Navigate();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось перейти на предыдущую страницу");
+                PageNow += 1;
+            }
+        }
+
+        public void NavigateToRight(object sender, RoutedEventArgs e)
+        {
+            PageNow += 1;
+            try
+            {
+                Navigate();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось перейти на следующую страницу");
+                PageNow -= 1;
+            }
+        }
+
+        public void Navigate()
+        {
+            string nameTypePages = "PresentationLibraryFunctions.Pages." + ((Pages)PageNow).ToString();
+            Type typePages = Type.GetType(nameTypePages);
+            MyFrame.Navigate(Activator.CreateInstance(typePages));
+        }
+
+        enum Pages
+        {
+            CreateTableExamplePage,
+            AddRowExamplePage
+        }
+    }
+}
